3aeb5367b8be475342ffabdf7753dbe84d70e8b8
TrentonYo <trentonyo@gmail.com>
2024-08-05 20:08:50 -0700

    Revert "Generated default example"
    This reverts commit f150724f3fcf328c875de1c772581418812fa699.

--- startout-react-app/src/exports.js
+++
@@ -1,4 +0,0 @@
-import Landing from "./pages/landing/landing"
-import Error404 from "./pages/error404/error404"
-
-export { Landing, Error404 }
--- startout-react-app/src/pages/landing/landing.jsx
+++
@@ -1,9 +0,0 @@
-export default function Landing() {
-    return (
-        <div className="bg-neutral-main h-screen w-screen text-neutral-light">
-            <h1 className="h-screen flex justify-center items-center text-6xl">
-                Welcome to StartOut!
-            </h1>
-        </div>
-    )
-}
